e6c1df997625daf9329f1dc68f1fe18b0ed9a9ce
balajinagarajkumarvolvo <balaji.nagaraj.kumar.2@volvocars.com>
2022-02-04 20:18:50 +0100

    IATW Copy of Smartidly Website

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./**.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        heroSection:
+          "linear-gradient(90deg, rgba(0,0,0,0.5) 0%, rgba(255,255,255,0.5) 100%), url('/assets/heroSection.jpeg')",
+        productSection:
+          "linear-gradient(90deg, rgba(255,255,255,0.9) 0%, rgba(255,255,255,0.9) 100%), url('/assets/dishplate.png')",
+      },
+    },
+    fontFamily: {
+      sans: "Inter",
+      megrim: "Megrim",
+    },
+  },
+  plugins: [],
+};
