30739b0bcb885672af950cce323e9429e6dc313b
Sanjay Kumar Kumaravelan <sanjaykumarcena@gmail.com>
2022-02-14 15:57:04 +0530

    IATW implements DM on register

---
+++ src/js/register.js
@@ -0,0 +1,60 @@
+const navbar = document.getElementById("navbar");
+const toggleButton = document.querySelector("#toggle");
+const navbarLinks = document.querySelector("#navbarLinks");
+const darkModeToggle = document.querySelector("#theme_icon");
+const html = document.querySelector("html");
+var mode = true;
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("left-0");
+  navbarLinks.classList.toggle("left-full");
+});
+
+window.onscroll = () => {
+  if (window.scrollY > 0) {
+    if (mode) {
+      navbar.classList.add("bg-white");
+    } else {
+      navbar.classList.add("bg-[#272727]");
+    }
+    navbar.classList.add("boxshadow");
+  } else {
+    if (mode) {
+      navbar.classList.remove("bg-white");
+    } else {
+      navbar.classList.remove("bg-[#272727]");
+    }
+    navbar.classList.remove("boxshadow");
+  }
+};
+
+darkModeToggle.addEventListener("click", () => {
+  html.classList.toggle("dark");
+  mode = !mode;
+  navbar.classList.remove("bg-white");
+  navbar.classList.remove("bg-[#272727]");
+});
+
+const navbar_logo = document.getElementById("navbar_logo");
+const themeIcon = document.getElementById("theme_icon");
+const rightSideLogo = document.getElementById("rightside_logo");
+
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.type === "attributes" && mutation.attributeName === "class") {
+      if (navbar_logo.src.includes("/assets/datatorlogo.svg")) {
+        // dark to light
+        navbar_logo.setAttribute("src", "/assets/datatorlogo-dark.svg");
+        themeIcon.setAttribute("src", "/assets/lightmodeicon.svg");
+        rightSideLogo.setAttribute("src", "assets/rightSideLogo-dark.svg");
+      } else {
+        // light to dark
+        navbar_logo.setAttribute("src", "/assets/datatorlogo.svg");
+        themeIcon.setAttribute("src", "/assets/darkmodeicon.svg");
+        rightSideLogo.setAttribute("src", "assets/rightSideLogo.svg");
+      }
+    }
+  });
+});
+
+observer.observe(document.querySelector("html"), { attributes: true });
